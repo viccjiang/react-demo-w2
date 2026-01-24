c5761e3fd3f394987eb0c12f1bdfb0778420e83d
jiangvicc <binbinbb520@gmail.com>
2026-01-24 14:51:55 +0800

    fix: update base path configuration for production environment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/react-demo-w2/' : '/',
   plugins: [react(),  tailwindcss()],
 })
